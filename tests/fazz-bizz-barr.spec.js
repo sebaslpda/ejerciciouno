adf2ef61f0d0da7b7cf3805f7c0253629f966b62
Sebita <sebastian.e.vogel@live.com>
2019-07-26 09:34:03 -0300

    se modifica codigo en fazz-bizz-barr se modifica el codigo que no se habia guardado, da resultado sin espacios entre las palabras entregadas

---
+++ tests/fazz-bizz-barr.spec.js
@@ -0,0 +1,31 @@
+function multiploDeTres(numero){
+    return (numero % 3) === 0;
+}
+
+function multiploDeCinco(numero){
+    return (numero % 5) === 0;
+}
+
+function multiploDeSiete(numero){
+    return (numero % 7) === 0;
+}
+
+function prueba(numero){
+   let resultado = ""
+   
+    if (multiploDeTres(numero)){
+        resultado = "Fazz";
+    }
+    if (multiploDeCinco(numero)){
+        resultado = resultado + "Bizz";
+    }
+    if (multiploDeSiete(numero)){
+        resultado = resultado + "Barr";
+    }
+   if (resultado = ""){
+       return numero
+   }
+    
+    return resultado
+    }
+ 
